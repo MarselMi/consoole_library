cca49adcc0f8e53e1be9b2a6cc8856d9a35902d3
Marsel Mikhailov <107858665+MarselMi@users.noreply.github.com>
2024-11-29 09:16:32 +0500

    Update validate.py

--- validate.py
+++ validate.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 
+from values import MAX_LENGHT
 from models import Book
 
 
@@ -8,13 +9,14 @@ class Validator:
     validator = Validator()
     validator.book_create_validate(book_instance: Book)
     Для валидации данных при создании необходимо инициализировать
-    класс Validator, затем ввзвать метод book_create_validate()
+    класс Validator, затем ввзвать метод .book_create_validate
     передав в него обьет класса Book
     """
 
     def book_create_validate(self, obj_validate: Book):
         self.check_instance_obj(obj_validate)
         self.check_year(obj_validate)
+        self.check_lenght_str_fields(obj_validate)
 
     @staticmethod
     def check_instance_obj(obj_validate: Book | None):
@@ -32,3 +34,12 @@ class Validator:
 
         if obj_validate.year < 0:
             raise ValueError("Год издания не может быть отрицательным")
+            
+    @staticmethod
+    def check_lenght_str_fields(obj_validate: Book):
+        for key, value in obj_validate.items():
+            if isinstance(value, str) and len(value) > MAX_LENGHT:
+                raise IndexError(f"В поле {key} превышение максимальной допустимой длинны строки")
+                
+        
+        
