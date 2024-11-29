cf4d0f01b66276a9e960a0b7e3d54a6c6a6357ad
Marsel Mikhailov <107858665+MarselMi@users.noreply.github.com>
2024-11-29 11:08:43 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-import crud
+import argparse
+
+from crud import CreateBook, update_book, delete_book
+
+
+def main():
+    commands = ("new","update","del")
+    information = argparse.ArgumentParser(description="CLI-приложение библиотеки")
+    add_arguments = information.add_argument(
+        "input", 
+        help("""
+        Для создания книги введите команду: "new"/n
+        Для обновления: "update -id" где id это id книги/n
+        Для удаления: "del -id" где id это id книги/n
+        Для выхода: комбинация клавишь "ctrl+c"
+        """)
+    )
+    input_arg = information.parse_args()[1]
+    
+    if input_arg not in commands:
+        add_arguments()
+    
+    
+if __name__ == "__main__":
+    main()
